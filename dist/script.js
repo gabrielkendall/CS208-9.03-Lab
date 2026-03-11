6d705db28516754a1a8ee4c62024e0d12107fe40
Gabriel Kendall <gabrielkendall@u.boisestate.edu>
2026-03-11 16:27:12 +0000

    comments

--- dist/script.js
+++ dist/script.js
@@ -1,22 +1,22 @@
 document.getElementById("lineupForm").addEventListener("submit", myLineUp);
 
 function myLineUp(event) {
-  event.preventDefault();
 
-  let status = document.querySelector('input[name="status"]:checked').value;
-  let gameDate = document.getElementById("gameDate").value;
+    // Prevents page reload
+    event.preventDefault();
 
-  let checkedPlayers = document.querySelectorAll('input[name="players"]:checked');
-  let players = [];
+    // Pulls the data
+    let status = document.querySelector('input[name="status"]:checked').value;
+    let gameDate = document.getElementById("gameDate").value;
+    let checkedPlayers = document.querySelectorAll('input[name="players"]:checked');
+    let players = [];
 
-  checkedPlayers.forEach(function(player) {
-    players.push(player.value);
-  });
+    // Formats the Player data
+    checkedPlayers.forEach(function(player) {
+        players.push(player.value);
+    });
 
-  console.log(status);
-  console.log(gameDate);
-  console.log(players);
-
-  document.getElementById("output").textContent =
-    "Status: " + status + ", Date: " + gameDate + ", Players: " + players.join(", ");
+    // Sets the output paragraph to the output values
+    document.getElementById("output").textContent =
+        "Status: " + status + ", Date: " + gameDate + ", Players: " + players.join(", ");
 }
